8284058aed5f03f2affd7f461801cc70401607a1
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-20 20:48:17 +0100

    [TASK] created login

---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {LoginResponse} from "../../models/login-response";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private http: HttpClient) {
+  }
+
+  login(username: string, password: string) {
+    // Send a request to the server to authenticate the user
+    return this.http.post<LoginResponse>('/api/login', {username, password}).subscribe(response => {
+      // Save the response token into local storage
+      const token = response.access_token;
+      localStorage.setItem('token', token);
+    });
+  }
+
+  logout() {
+    // Send a request to the server to log out the user
+    return this.http.post('/api/logout', {}).subscribe(() => {
+      // Remove login data from local storage
+      localStorage.removeItem('token');
+    });
+  }
+}
