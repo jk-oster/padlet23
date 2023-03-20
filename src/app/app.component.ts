8871c5e5059ba3c319f151628aae894df2d88e5d
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-20 20:49:39 +0100

    [TASK] updated app module & components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bs-root',
+  // we can use the selector to use this component in other components
+  // for example <tw-root></tw-root> is used in index.html
+  // the prefix tw is defined in angular.json
+  selector: 'tw-root',
+  // template url is the html file that will be used for this component
   templateUrl: './app.component.html',
+  // styles is the css file that will be used for this component
   styles: []
 })
 export class AppComponent {
-  title = 'bookstore23';
+  // title is a property of this component
+  title = 'padlet23';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
 
-import { AppComponent } from './app.component';
+import {HttpInterceptorService} from "./core/interceptors/http-interceptor.service";
+import {AppRoutingModule} from './core/app-routing.module';
+import {SharedModule} from './shared/shared.module';
+import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {LoginComponent} from './features/login/login.component';
+import {AppTopBarComponent} from './features/app-top-bar/app-top-bar.component';
 
 @NgModule({
+  // what components, directives, and pipes belong to the module
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppTopBarComponent
   ],
+  // what other modules are needed to run this module
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule
   ],
-  providers: [],
+  // what services does this module provide
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true}
+  ],
+  // what is the main component of this module
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
