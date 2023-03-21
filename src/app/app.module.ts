1e9ef98c666aed841120ac464f6fdcd2a9c55e4c
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-21 01:38:00 +0100

    [TASK] component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,41 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {CommonModule} from '@angular/common';
 import {AppComponent} from './app.component';
 
 import {HttpInterceptorService} from "./core/http-interceptor.service";
+import {HttpClientModule} from '@angular/common/http';
+
 import {AppRoutingModule} from './core/app-routing.module';
 import {SharedModule} from './shared/shared.module';
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {LoginComponent} from './features/login/login.component';
 import {AppTopBarComponent} from './features/app-top-bar/app-top-bar.component';
+import {PadletCardComponent} from './features/padlet-card/padlet-card.component';
+import {PadletIndexComponent} from './features/padlet-index/padlet-index.component';
+import {PadletDetailComponent} from './features/padlet-detail/padlet-detail.component';
+import {PostCardComponent} from './features/post-card/post-card.component';
+import {NotFoundComponent} from './features/not-found/not-found.component';
 
 @NgModule({
   // what components, directives, and pipes belong to the module
   declarations: [
     AppComponent,
     LoginComponent,
-    AppTopBarComponent
+    AppTopBarComponent,
+    PadletCardComponent,
+    PadletIndexComponent,
+    PadletDetailComponent,
+    PostCardComponent,
+    NotFoundComponent
   ],
   // what other modules are needed to run this module
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    HttpClientModule,
+    CommonModule,
+    SharedModule,
   ],
   // what services does this module provide
   providers: [
