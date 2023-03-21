53476624891497db7c42e7127159c7a9bb2a7b4b
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-21 01:38:25 +0100

    [TASK] padlet, post and 404 routes

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "../features/login/login.component";
+import {PadletIndexComponent} from "../features/padlet-index/padlet-index.component";
+import {NotFoundComponent} from "../features/not-found/not-found.component";
+import {PadletDetailComponent} from "../features/padlet-detail/padlet-detail.component";
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  {path: '', component: PadletIndexComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'padlet/:id', component: PadletDetailComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
