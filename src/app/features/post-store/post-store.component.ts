017e7c032269cd84b3bc5d3e32fcbc5890f6b634
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-24 16:52:34 +0100

    [TASK] updated post store component

--- src/app/features/post-store/post-store.component.ts
+++ src/app/features/post-store/post-store.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import {Location} from "@angular/common";
 import {AuthService} from "../../core/auth.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PostService} from "../../core/post.service";
+import {Padlet} from "../../models/padlet";
+import {ImageService} from "../../core/image.service";
 
 @Component({
   selector: 'tw-post-store',
@@ -13,34 +14,49 @@ import {PostService} from "../../core/post.service";
 })
 export class PostStoreComponent implements OnInit {
   storeForm: FormGroup = new FormGroup({});
+
+  @Input() padlet: Padlet = {
+    id: 0,
+    name: '',
+    cover: '',
+    description: '',
+    public: true,
+    created_at: '',
+    updated_at: '',
+    user_id: 0,
+  }
+
   padletId: number = 0;
 
   constructor(
-    private location: Location,
     protected auth: AuthService,
     private router: Router,
     private postService: PostService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private imageService: ImageService
   ) {
     this.auth = auth;
   }
 
   ngOnInit(): void {
-    this.padletId = Number(this.route.snapshot.paramMap.get('id'));
+    if(this.route.snapshot.paramMap.get('id')) {
+      this.padletId = Number(this.route.snapshot.paramMap.get('id'));
+    }
+    else {
+      this.padletId = this.padlet.id;
+    }
     this.storeForm = new FormGroup({
       content: new FormControl(),
-      cover: new FormControl(),
+      cover: new FormControl('https://source.unsplash.com/random/800x600/?' + this.imageService.randomTopic()),
     });
   }
 
   store() {
     this.postService.createPost(this.padletId, this.storeForm.value).subscribe((response: any) => {
       console.log(response);
-      this.location.back();
+      if(response.id) {
+        this.padlet.posts?.push(response);
+      }
     });
-  }
-
-  back() {
-    this.location.back();
   }
 }
