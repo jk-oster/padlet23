d24c3e10519c41fdb6817d3084b5b2f40023a04d
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-24 16:52:57 +0100

    [TASK] updated padlet store component to suggest random images

--- src/app/features/padlet-store/padlet-store.component.ts
+++ src/app/features/padlet-store/padlet-store.component.ts
@@ -4,6 +4,7 @@ import {FormControl, FormGroup} from "@angular/forms";
 import {AuthService} from "../../core/auth.service";
 import {Router} from "@angular/router";
 import {PadletService} from "../../core/padlet.service";
+import {ImageService} from "../../core/image.service";
 
 @Component({
   selector: 'tw-padlet-store',
@@ -13,15 +14,19 @@ import {PadletService} from "../../core/padlet.service";
 export class PadletStoreComponent implements OnInit {
   storeForm: FormGroup = new FormGroup({});
 
-  constructor(private location: Location, protected auth: AuthService, private router: Router, private padletService: PadletService) {
-    this.auth = auth;
+  constructor(
+    private location: Location,
+    private imageService: ImageService,
+    protected auth: AuthService,
+    private router: Router,
+    private padletService: PadletService) {
   }
 
   ngOnInit(): void {
     this.storeForm = new FormGroup({
       name: new FormControl(),
       description: new FormControl(),
-      cover: new FormControl(),
+      cover: new FormControl('https://source.unsplash.com/random/800x600/?' + this.imageService.randomTopic()),
     });
   }
 
