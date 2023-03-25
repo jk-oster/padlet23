3f9e8a07687aeda913c9a7f1354f31507eb8e4d2
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-03-25 20:22:20 +0100

    [TASK] updated model to API response

--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,15 +1,19 @@
-import {Rating} from "./rating";
 import {User} from "./user";
 
 export interface Post {
   id: number;
   content: string;
   cover: string;
+
   created_at: string;
   updated_at: string;
+
   user_id: number;
   user?: User;
-  ratings?: Rating[];
-  comments?: Comment[];
 
+  comments_count: number;
+  comments: Comment[];
+  rating: number;
+  rating_count: number;
+  user_rating: number;
 }
